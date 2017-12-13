ce80d28ac7e3dbaa73279b5a8cb89c2cfcb78c36
GutierrezLiliana <liliana_gutierrez@davincischools.org>
2017-12-13 13:08:42 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function resetAll(){
+	document.getElementById("one").classList.remove("mediumorchid");
+	document.getElementById("two").classList.remove("mediumorchid");
+	document.getElementById("three").classList.remove("mediumorchid");
+	document.getElementById("four").classList.remove("mediumorchid");
+	document.getElementById("one").innerHTML = "";
+	document.getElementById("two").innerHTML = "";
+	document.getElementById("three").innerHTML = "";
+	document.getElementById("four").innerHTML = "";
+
+}
+
+function showText(elem){
+	elem.innerHTML = "Sunsets";
+}
+function s1(elem) {
+    document.getElementById("one").classList.toggle("sunset2");
+	document.getElementById("nature").play();
+}
+function s3(elem) {
+    document.getElementById("two").classList.toggle("sunset5");
+}
+function e2(elem) {
+    document.getElementById("three").classList.toggle("eclipse3");
+}
+function s4(elem) {
+    document.getElementById("four").classList.toggle("sunset6");
+}
